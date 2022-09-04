54db5d57fe77d6513b6a48153f14f03d3808a57e
fabbitox <hyunjin_11@naver.com>
2022-09-04 23:01:10 +0900

    implementation of delete button

--- app/src/main/java/com/first/minesweeperpr/Statistics.java
+++ app/src/main/java/com/first/minesweeperpr/Statistics.java
@@ -1,9 +1,11 @@
 package com.first.minesweeperpr;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Statistics extends AppCompatActivity {
@@ -35,7 +37,7 @@ public class Statistics extends AppCompatActivity {
         for (int i = 0; i < 3; i++) {
             String best_msg = "최고기록: ";
             if (best_recs[i] != -1) {
-                best_msg = best_msg + best_recs[i];
+                best_msg = best_msg + best_recs[i] + "초";
             }
             else {
                 best_msg = best_msg + "-";
@@ -49,13 +51,38 @@ public class Statistics extends AppCompatActivity {
 
             String rate_msg = "승률: ";
             if (total_recs[i] != 0) {
-                double win_rate = (double)win_recs[i] / total_recs[i];
+                double win_rate = (double)win_recs[i] / total_recs[i] * 100;
                 rate_msg = rate_msg + String.format(getString(R.string.fraction), win_rate);
             }
             else {
                 rate_msg = rate_msg + "-";
             }
             rate_tvs[i].setText(rate_msg);
+        }
+
+        ImageButton delBtn = findViewById(R.id.del_rec_btn);
+        delBtn.setOnClickListener(v -> {
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(Statistics.this);
+            alertBuilder.setTitle("delete check");
+            alertBuilder.setMessage("기록을 삭제하시겠습니까?");
+            alertBuilder.setPositiveButton("예", (dialog, which) -> delete_recs(best_tvs, total_tvs, win_tvs, rate_tvs, shp, shp_keys));
+            alertBuilder.setNegativeButton("아니요", (dialog, which) -> {});
+            alertBuilder.show();
+        });
+    }
+
+    private void delete_recs(TextView[] best_tvs, TextView[] total_tvs, TextView[] win_tvs, TextView[] rate_tvs, SharedPreferences shp, String[] shp_keys) {
+        SharedPreferences.Editor editor = shp.edit();
+        for (int i = 0; i < 9; i++) {
+            editor.remove(shp_keys[i]);
+        }
+        editor.apply();
+
+        for (int i = 0; i < 3; i++) {
+            best_tvs[i].setText("최고기록: -");
+            total_tvs[i].setText("전체 게임: 0");
+            win_tvs[i].setText("승리한 게임: 0");
+            rate_tvs[i].setText("승률: -");
         }
     }
 }
